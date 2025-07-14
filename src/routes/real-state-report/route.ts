a0b36a3353ac67330cfadc586487ead0eaa0e398
Andre Ponce <asponceg@unah.hn>
2025-07-14 07:00:14 -0600

    sending email to user

--- src/routes/real-state-report/route.ts
+++ src/routes/real-state-report/route.ts
@@ -6,6 +6,8 @@ import { UserJWT } from '@/src/middlewares/authMiddleware'
 import { validateRequest } from '@/src/middlewares/validate-request'
 import RealStateReport from '@/src/schemas/real-state-reports.schemas'
 import { RealStateModel } from '@/src/schemas/real-state.schemas'
+import { userModel } from '@/src/schemas/user.schemas'
+import { EmailService } from '@/src/services/email/email.service'
 import { TokenManager } from '@/src/utils/JWT/tokens-manager'
 import { reportPropertySchema } from '@/src/zod/report-property'
 import { Request, Response, Router } from 'express'
@@ -29,6 +31,42 @@ reportsRouter.post('/real-state',
     if (accessToken) {
       decoded = TokenManager.verifyToken(accessToken) as UserJWT
     }
+
+    const emailService = new EmailService()
+
+    if (decoded) {
+      const user = await userModel.findById(decoded.userId)
+      if (user?.email) {
+        await emailService.sendEmail({
+          provider: 'sendgrid',
+          to: {
+            email: user.email,
+            name: user.name
+          },
+          subject: 'Reporte de propiedad',
+          html: `
+            <h1>Hola ${user.name},</h1>
+            <p>Hemos recibido un reporte de propiedad por el motivo de ${reason}.</p>
+            <p>De la siguiente URL: ${reservationsUrl}</p>
+            <p>Gracias por tu ayuda. Revisaremos la propiedad lo antes posible y la eliminaremos si es necesario.</p>
+          `
+        })
+      }
+    }
+
+    emailService.sendEmail({
+      provider: 'nodemailer',
+      to: {
+        email: 'asponceg@gmail.com',
+        name: 'André Ponce CEO & Founder'
+      },
+      subject: 'Reporte de propiedad',
+      html: `
+        <p>Hola mi poderisisimo fundador, </p>
+        <p>Un usuario ha reportado la propiedad <strong>${realState.title}</strong> por el motivo de <strong>${reason}</strong> dijo: <pre>${message}</pre></p>
+        <p>De la siguiente URL: ${reservationsUrl}</p>
+      `,
+    })
 
     try {
       await RealStateReport.create({
