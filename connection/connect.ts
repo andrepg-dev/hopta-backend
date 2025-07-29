66671bf99f4267c9ae79ea6445266019aa59ff58
André Ponce <asponceg@gmail.com>
2025-07-29 13:09:52 -0600

    backend URI

--- connection/connect.ts
+++ connection/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import { CONNECTIONS } from '../constants/connection.constants'
 import Logs from '@/src/services/logs/save-logs.service'
 
-const connectionString = `mongodb+srv://asponceg:${CONNECTIONS.PASSWORD}@hopta-01-testing.a2r5i.mongodb.net/${CONNECTIONS.DATABASE_NAME}?retryWrites=true&w=majority&appName=hopta-01-testing`
+const connectionString = `mongodb+srv://admin:${CONNECTIONS.PASSWORD}@hopta.g94msvw.mongodb.net/${CONNECTIONS.DATABASE_NAME}?retryWrites=true&w=majority&appName=hopta`
 
 export const connectToDatabase = async () => {
   try {
