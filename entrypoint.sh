1be85d75501d59ae3878765595cfc560bb659c09
André Ponce <asponceg@gmail.com>
2025-09-26 11:40:00 -0600

    Settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,19 @@ if [ ! -f /etc/letsencrypt/live/backend.hopta.hn/fullchain.pem ]; then
   echo ">>> Certificados generados, apagando Nginx temporal"
   nginx -s stop
 
+  # ⚡️ Generar configs SSL adicionales si no existen
+  if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
+    echo ">>> Creando options-ssl-nginx.conf"
+    wget https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem \
+         -O /etc/letsencrypt/ssl-dhparams.pem
+    wget https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/options-ssl-nginx.conf \
+         -O /etc/letsencrypt/options-ssl-nginx.conf
+  fi
+
   # Reemplazar config por la definitiva con SSL
   cp /app/nginx/backend.conf /etc/nginx/conf.d/default.conf
 fi
 
-# Arrancar backend + loop de renovación + Nginx
 node dist/src/index.js &
 
 while :; do
